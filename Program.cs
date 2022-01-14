d0bec42ac14b8d09bf461bde7b06f07fcc1d251f
Sukalyan <sukalyandash1920@gmail.com>
2022-01-14 22:49:04 +0530

    Program File Added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+
+            UC2_AddTheDetails addTheDetails = new UC2_AddTheDetails();
+            addTheDetails.EnterInput();
+            addTheDetails.ViewTheDetails();
+            //addTheDetails.PrintTheDetails();
+
+            UC3_EditDetails editDetails = new UC3_EditDetails();
+            editDetails.EditUsingName();
+
+            UC4_DeleteDetails deleteDetails = new UC4_DeleteDetails();
+            deleteDetails.DeletetheName();
+
+        }
+    }
+}
